8f00dadb40f80211ae9c2d9e6187527e93847295
Samuel Fernandes <samuelfernandesfilho2007@gmail.com>
2025-01-17 13:43:06 -0300

    Criando as páginas do menu

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,3 +6,23 @@ home = Blueprint('home', __name__)
 def homepage():
     return render_template('index.html')
 
+@home.route('/jogo_form', methods = ['GET', 'POST'])
+def jogo_form():
+    return render_template('jogo_form.html')
+
+@home.route('/biblioteca', methods = ['GET', 'POST'])
+def biblioteca():
+    return render_template('biblioteca.html')
+
+@home.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')
+
+@home.route('/historia')
+def historia():
+    return render_template('historia.html')
+
+@home.route('/creditos')
+def creditos():
+    return render_template('creditos.html')
+
