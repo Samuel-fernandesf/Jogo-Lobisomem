b8a79d7e2a29843db6152bd51b0e21c418eb29f9
ImLuizz <luizgabriellelipereira@gmail.com>
2025-01-21 20:42:49 -0300

    Criando paginas de resumo e ação

--- app/routes/jogo.py
+++ app/routes/jogo.py
@@ -3,6 +3,10 @@ from flask import Blueprint, render_template, redirect, url_for, request
 
 jogo = Blueprint('jogo', __name__)
 
-@jogo.route('/')
-def jogo_01():
-    return render_template('jogo_01.html')
+@jogo.route('/resumo_rodada')
+def resumo_rodada():
+    return render_template('resumo.html')
+
+@jogo.route('/acao/<id:int>', methods=['GET', 'POST'])
+def acao():
+    return render_template('acao.html')
