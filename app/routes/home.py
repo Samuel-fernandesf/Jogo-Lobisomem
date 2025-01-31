11ba0ea4977856fac3195b77911386ec734afc6c
Qulszy <yfernades07@gmail.com>
2025-01-31 15:14:32 -0300

    Correção bugs açou/jogo

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,10 +6,6 @@ home = Blueprint('home', __name__)
 def homepage():
     return render_template('index.html')
 
-@home.route('/biblioteca', methods=['GET', 'POST'])
-def biblioteca():
-    return render_template('biblioteca.html', jogadores=session.get('jogadores', []))
-
 @home.route('/tutorial')
 def tutorial():
     return render_template('tutorial.html')
