69053e7645ae7b1d0cdfb0ea0b89b5186f2b6549
ImLuizz <luizgabriellelipereira@gmail.com>
2025-02-06 17:22:46 -0300

    mais conflitos

--- app/index.py
+++ app/index.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from app import app
-
-if __name__ == '__main__':
-=======
 from app import app
 from flask import session
 
@@ -11,5 +6,4 @@ def clear_all_sessions():
     session.clear()  # Limpa a sessão atual de qualquer usuário
     
 if __name__ == '__main__':
->>>>>>> e20e3d3 (css | bugs)
     app.run(debug=True, host='0.0.0.0', port=5000)
