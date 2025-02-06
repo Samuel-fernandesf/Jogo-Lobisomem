ffc80e2cd5fc7a2b691f6ad56c9b86d8eff462c4
ImLuizz <luizgabriellelipereira@gmail.com>
2025-02-06 17:22:11 -0300

    mais conflitos

--- app/index.py
+++ app/index.py
@@ -1,4 +1,15 @@
+<<<<<<< HEAD
 from app import app
 
 if __name__ == '__main__':
+=======
+from app import app
+from flask import session
+
+@app.before_first_request
+def clear_all_sessions():
+    session.clear()  # Limpa a sessão atual de qualquer usuário
+    
+if __name__ == '__main__':
+>>>>>>> e20e3d3 (css | bugs)
     app.run(debug=True, host='0.0.0.0', port=5000)
--- app/static/js/acougueiro.js
+++ app/static/js/acougueiro.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let container = document.getElementsByClassName('container')
     let botaoRevelar = document.getElementById("revelar-papel");
     let divPapel = document.getElementById("papel");
-    let divBotao = document.getElementById("botoes")
-    let divEntrega = document.getElementById('entregar-dispositivo')
+    let divBotao = document.getElementById("botoes");
+    let divEntrega = document.getElementById('entregar-dispositivo');
+    let divMensagem = document.getElementById("mensagem")
     if (!botaoRevelar || !divPapel) return; 
 
     botaoRevelar.addEventListener("click", function () {
-        divEntrega.style.display = 'none'
+        divEntrega.style.display = 'none';
         divPapel.style.display = "block"; 
         divBotao.style.display = "block";
-
+        divMensagem.style.display = "block";
+        console.log(divMensagem)
     });
 
 });
